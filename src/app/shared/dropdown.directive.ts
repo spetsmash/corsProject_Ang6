22cab3490a9303620e56950838a3985696fe01b8
spetsmash <spetsmash@list.ru>
2018-10-10 12:36:34 +0300

    8/95 Building and Using a Dropdown Directive      Section 8, Lecture 95

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,9 +1,20 @@
-import { Directive } from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
 
-export class DropdownDirective {
+export class DropdownDirective implements OnInit {
+  @HostBinding('class.open') isOpen = false;
+  constructor(private elRef: ElementRef,
+              private renderer: Renderer2) {}
+
+    ngOnInit() {}
+
+    @HostListener('click')
+      toggleOpen() {
+        this.isOpen = !this.isOpen;
+      }
+
 
 }
