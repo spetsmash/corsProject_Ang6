d28bac86c0a300bfea1d230726f647221d2002f4
spetsmash <spetsmash@list.ru>
2018-11-02 11:21:27 +0200

    20/261 Signing Users Up            Section 20, Lecture 261

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,10 +1,14 @@
 import {Injectable} from '@angular/core';
+import * as firebase from 'firebase';
 
 @Injectable()
 export class AuthService {
 
   constructor() {}
   signupUser(email: string, password: string) {
-
+    firebase.auth().createUserWithEmailAndPassword(email, password).
+      catch(
+        error => console.log(error)
+    );
   }
 }
