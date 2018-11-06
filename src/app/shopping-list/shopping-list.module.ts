ff8203ce956945e0cc27a52942bf6ce631c8ab93
spetsmash <spetsmash@list.ru>
2018-11-06 15:06:43 +0200

    21/279 Creating a Shopping List Feature Module        Section 21, Lecture 279

--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -2,7 +2,6 @@ import {NgModule} from '@angular/core';
 import {ShoppingListComponent} from './shopping-list.component';
 import {ShoppingEditComponent} from './shopping-edit/shopping-edit.component';
 import {CommonModule} from '@angular/common';
-import {RouterModule} from '@angular/router';
 import {FormsModule} from '@angular/forms';
 
 @NgModule({
